9536281e9ec4376c47f89738bf4f427d4b9738be
David Andrea <davejandrea@gmail.com>
2024-03-16 21:32:16 -0300

    make grid a class property so it can be referenced in other functions

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,6 +2,7 @@ export class Board {
   width;
   height;
   blocks = [];
+  grid = []
 
   constructor(width, height) {
     this.width = width;
@@ -17,18 +18,17 @@ export class Board {
   }
 
   toString() {
-    const grid = [];
     for (let i = 0; i < this.height; i++) {
-      grid[i] = [];
+      this.grid[i] = [];
       for (let j = 0; j < this.width; j++) {
-        grid[i][j] = ".";
+        this.grid[i][j] = ".";
       }
     }
     for (let block of this.blocks) {
-      grid[block[1]][block[2]] = block[0];
+      this.grid[block[1]][block[2]] = block[0];
     }
     // convert grid to text for bottom
-    let strings = grid.map(row => row.join(""));
+    let strings = this.grid.map(row => row.join(""));
     return strings.join("\n") + "\n";
   }
 }
