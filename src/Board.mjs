00a89a7f58a7583de623b86ddcc9a66a42eb1061
David Andrea <davejandrea@gmail.com>
2024-03-16 22:35:07 -0300

    implemented tick()

--- src/Board.mjs
+++ src/Board.mjs
@@ -3,6 +3,7 @@ export class Board {
   height;
   blocks = [];
   grid = []
+  movingBlock = null;
 
   constructor(width, height) {
     this.width = width;
@@ -17,18 +18,22 @@ export class Board {
   }
 
   drop(block) {
-    this.blocks.push([block, 0, 1])
+    this.grid[0][1] = block;
+    this.movingBlock = [block, 1, 0];
   }
 
   tick() {
+    this.grid[this.movingBlock[2]][this.movingBlock[1]] = ".";
+    this.grid[this.movingBlock[2] + 1][this.movingBlock[1]] = this.movingBlock[0];
 
   }
 
   toString() {
 
-    for (let block of this.blocks) {
-      this.grid[block[1]][block[2]] = block[0];
-    }
+    // for (let block of this.blocks) {
+    //   this.grid[block[1]][block[2]] = block[0];
+    // }
+
     // convert grid to text for bottom
     let strings = this.grid.map(row => row.join(""));
     return strings.join("\n") + "\n";
