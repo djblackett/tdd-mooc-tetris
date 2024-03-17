74942380ef7548f1f841189cf58ae2366dc7f4e0
David Andrea <davejandrea@gmail.com>
2024-03-16 22:36:28 -0300

    cleaned up toString() method. No functionality change

--- src/Board.mjs
+++ src/Board.mjs
@@ -29,12 +29,6 @@ export class Board {
   }
 
   toString() {
-
-    // for (let block of this.blocks) {
-    //   this.grid[block[1]][block[2]] = block[0];
-    // }
-
-    // convert grid to text for bottom
     let strings = this.grid.map(row => row.join(""));
     return strings.join("\n") + "\n";
   }
