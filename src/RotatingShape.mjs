47fff66e3a63b27183ab0d92e4efb9498f01ed8b
David Andrea <davejandrea@gmail.com>
2024-03-18 21:48:18 -0300

    add helper method to create grid from string

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -10,14 +10,19 @@ export class RotatingShape {
   }
 
   rotateRight() {
+    let newArr = this.getMatrixFromString()
+    newArr = newArr[0].map((val, index) => newArr.map(row => row[index]).reverse());
+    let strings = newArr.map(row => row.join(""));
+    return strings.join("\n") + "\n";
+  }
+
+  getMatrixFromString() {
     let shapeArr = this.shape.substring(0, this.shape.length - 1).split("\n");
     let newArr = []
     for (let i of shapeArr) {
       newArr.push(i.split(""));
     }
-    newArr = newArr[0].map((val, index) => newArr.map(row => row[index]).reverse());
-    let strings = newArr.map(row => row.join(""));
-    return strings.join("\n") + "\n";
+    return newArr;
   }
 
   toString() {
