10b9ebef3a00b7bc5e0c9967a9588ec6877f902a
David Andrea <davejandrea@gmail.com>
2024-03-18 21:20:49 -0300

    began implementing rotateRight

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -9,6 +9,15 @@ export class RotatingShape {
     return new RotatingShape(str.replaceAll(" ", "") + "\n");
   }
 
+  rotateRight() {
+    let shapeArr = this.shape.split("\n");
+    let newArr = []
+    for (let i of shapeArr) {
+      newArr.push(i.split());
+    }
+    newArr[0].map((val, index) => newArr.map(row => row[index]).reverse())
+  }
+
   toString() {
     return this.shape;
   }
