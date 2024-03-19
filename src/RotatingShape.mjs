4675b4342d347327a18ae732605e5c5c2bc96846
David Andrea <davejandrea@gmail.com>
2024-03-18 21:40:26 -0300

    can be rotated right - passing

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -10,12 +10,14 @@ export class RotatingShape {
   }
 
   rotateRight() {
-    let shapeArr = this.shape.split("\n");
+    let shapeArr = this.shape.substring(0, this.shape.length - 1).split("\n");
     let newArr = []
     for (let i of shapeArr) {
-      newArr.push(i.split());
+      newArr.push(i.split(""));
     }
-    newArr[0].map((val, index) => newArr.map(row => row[index]).reverse())
+    newArr = newArr[0].map((val, index) => newArr.map(row => row[index]).reverse());
+    let strings = newArr.map(row => row.join(""));
+    return strings.join("\n") + "\n";
   }
 
   toString() {
