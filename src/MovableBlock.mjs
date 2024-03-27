5fd5327b78179e8376eca0d795d3bb67aec6dac0
David Andrea <davejandrea@gmail.com>
2024-03-27 11:20:46 -0300

    add isValid method to MovableBlock class now that it can access board dimensions

--- src/MovableBlock.mjs
+++ src/MovableBlock.mjs
@@ -17,6 +17,12 @@ export class MovableBlock {
     return new MovableBlock(this.shape, this.row + 1, this.col, this.boardWidth, this.boardHeight);
   }
 
+  isValid(x, y) {
+    if (x >= 0 && y >= 0 && x < this.boardWidth && y < this.boardHeight) {
+      return true;
+    }
+  }
+
   getFilledCoordinates() {
     const points = [];
     const shape = this.shape.shape;
