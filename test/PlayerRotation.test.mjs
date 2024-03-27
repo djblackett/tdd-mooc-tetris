245747104e00e32d01a03d46495fb4ccb963a408
David Andrea <davejandrea@gmail.com>
2024-03-27 18:39:41 -0300

    create test for player rotating blocks

---
+++ test/PlayerRotation.test.mjs
@@ -0,0 +1,24 @@
+import { beforeEach, describe } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("Moving tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+    board.drop(Tetromino.T_SHAPE)
+  });
+
+  test("a falling tetromino can be rotated", () => {
+    board.rotateRight()
+    expect(board.toString()).to.equalShape(
+      `....T.....
+       ....TT....
+       ....T.....
+       ..........
+       ..........
+       ..........`
+    );
+  })
+})
