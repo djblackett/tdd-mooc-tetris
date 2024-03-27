24ec54bf1eca4219dec3b927153e60dfa6a2b5eb
David Andrea <davejandrea@gmail.com>
2024-03-27 19:16:51 -0300

    add test for "it cannot be rotated when there is no room to rotate"

--- test/PlayerRotation.test.mjs
+++ test/PlayerRotation.test.mjs
@@ -21,4 +21,21 @@ describe("Moving tetrominoes", () => {
        ..........`
     );
   })
+
+  test("it cannot be rotated when there is no room to rotate", () => {
+    board.moveDown()
+    board.moveDown()
+    board.moveDown()
+    board.moveDown()
+    board.rotate()
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ..........
+       ....T.....
+       ...TTT....`
+    );
+  })
 })
