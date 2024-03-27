fa06d330f52bdfeb91c66e06627088ac101cbf37
David Andrea <davejandrea@gmail.com>
2024-03-27 18:51:22 -0300

    create rotate function in MovableBlock

--- test/PlayerRotation.test.mjs
+++ test/PlayerRotation.test.mjs
@@ -1,4 +1,4 @@
-import { beforeEach, describe } from "vitest";
+import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
