bad4a3d8be018b7988366ef4e6b92d3278bedeb6
David Andrea <davejandrea@gmail.com>
2024-04-02 20:08:41 -0300

    create ShuffleBag test

--- test/ShuffleBag.test.mjs
+++ test/ShuffleBag.test.mjs
@@ -2,10 +2,18 @@ import { beforeEach, describe, onTestFailed, test } from "vitest";
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
+import { ShuffleBag } from "../src/ShuffleBag.mjs";
 
 describe("ShuffleBag", () => {
 
 test("Tetrominos should be fed to board in random order", () => {
+  // todo add more block types after implementing
+  const Tetrominos = [Tetromino.T_SHAPE, Tetromino.O_SHAPE, Tetromino.I_SHAPE];
+  const shuffle1 = new ShuffleBag(Tetrominos).getShuffleBag();
+  const shuffle2 = new ShuffleBag(Tetrominos).getShuffleBag()
+
+  expect(shuffle1).not.toStrictEqual(shuffle2)
+
 
 })
 
