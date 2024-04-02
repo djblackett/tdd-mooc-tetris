b73e58ce5a6d8132febf09f0c7c9391c3132e105
David Andrea <davejandrea@gmail.com>
2024-04-02 17:33:06 -0300

    add test for observer

--- test/PlayerScore.test.mjs
+++ test/PlayerScore.test.mjs
@@ -1,14 +1,8 @@
 import { beforeEach, describe, onTestFailed, test } from "vitest";
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
-import { Tetromino } from "../src/Tetromino.mjs";
 import { setUpDoubleRowClear, setUpRowClear } from "./testing.mjs";
-
-function fallToBottom(board) {
-  for (let i = 0; i < 10; i++) {
-    board.tick();
-  }
-}
+import { PlayerScore } from "../src/PlayerScore.mjs";
 
 describe("Players can score points", () => {
   let board
@@ -49,5 +43,15 @@ describe("Players can score points", () => {
     const rowsCleared = board.checkRows()
     score += rowsCleared * 100;
     expect(score).toBe(400);
+  })
+
+
+  test("should register points earned with observer", () => {
+    const playerScore = new PlayerScore();
+    board.attach(playerScore);
+    setUpRowClear(board)
+    board.checkRows()
+    expect(playerScore.score).toBe(100);
+
   })
 })
