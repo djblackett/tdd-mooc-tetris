cbc8f587a1380d11b9a1429df09c1cc71de91ca7
David Andrea <davejandrea@gmail.com>
2024-04-02 19:45:38 -0300

    create ShuffleBag test

---
+++ test/ShuffleBag.test.mjs
@@ -0,0 +1,12 @@
+import { beforeEach, describe, onTestFailed, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("ShuffleBag", () => {
+
+test("Tetrominos should be fed to board in random order", () => {
+
+})
+
+})
