28b55b10253fe49494eb3b9046f0dff0fda171b8
David Andrea <davejandrea@gmail.com>
2024-04-02 19:36:49 -0300

    create ShuffleBag

--- src/ShuffleBag.mjs
+++ src/ShuffleBag.mjs
@@ -1,9 +1,16 @@
 class ShuffleBag {
   blocks;
+
   constructor(blockArray) {
     this.blocks = blockArray;
+    this.shuffle(this.blocks)
   }
+
+  // todo - add seed?
   shuffle() {
-    this.blocks = this.blocks.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value)
+    this.blocks = this.blocks
+      .map(value => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value)
   }
 }
